9cfdf69c3e31683e50e8c7031c36be2a26f6ab70
Fenali Vora <fenali@creolestudios.com>
2017-07-19 17:54:05 +0530

    - I have added the user pojo file.

---
+++ app/src/main/java/com/example/naresh/pubnubchat/pojo/User.java
@@ -0,0 +1,12 @@
+package com.example.naresh.pubnubchat.pojo;
+
+/**
+ * Created by admin1234 on 7/19/17.
+ */
+
+public class User {
+
+    public String userName;
+    public String lastName;
+    public int userId;
+}
