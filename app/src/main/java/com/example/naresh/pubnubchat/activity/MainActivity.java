b3e0f674090c05921c402b5649d4c680e9addae5
Naresh <naresh@creolestudios.com>
2017-07-20 14:48:46 +0530

    Shared Preferences added for managing user session for logout service. Logout Button added in Actionbar. Alert Dialog for Logout added as well.

--- app/src/main/java/com/example/naresh/pubnubchat/activity/MainActivity.java
+++ app/src/main/java/com/example/naresh/pubnubchat/activity/MainActivity.java
@@ -6,11 +6,15 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.naresh.pubnubchat.R;
+import com.example.naresh.pubnubchat.callback.CustomCallback;
 import com.example.naresh.pubnubchat.databinding.MainActivityBinding;
+import com.example.naresh.pubnubchat.fragment.ChatFragment;
+import com.example.naresh.pubnubchat.fragment.LoginFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements CustomCallback {
     private SharedPreferences sharedPreferences;
     private FragmentManager fragmentManager;
     private FragmentTransaction fragmentTransaction;
@@ -23,5 +27,39 @@ public class MainActivity extends AppCompatActivity {
 
         sharedPreferences = getSharedPreferences("details", MODE_PRIVATE);
         fragmentManager = getSupportFragmentManager();
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        changeLogin();
+    }
+
+    public void changeLogin() {
+        if (sharedPreferences.getString("username", null) == null) {
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.container, new LoginFragment(), "Login");
+            fragmentTransaction.commit();
+        } else {
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.container, new ChatFragment(), "Chat");
+            Toast.makeText(this, "LoggedIn Successfully...!!", Toast.LENGTH_SHORT).show();
+            fragmentTransaction.commit();
+        }
+    }
+
+    @Override
+    public void loginActivity(int LOGIN_STATE) {
+
+        if (LOGIN_STATE == 0) {
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.container, new ChatFragment(), "Chat");
+            fragmentTransaction.commit();
+        } else {
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.container, new LoginFragment(), "Login");
+            fragmentTransaction.commit();
+        }
     }
 }
--- app/src/main/java/com/example/naresh/pubnubchat/fragment/ChatFragment.java
+++ app/src/main/java/com/example/naresh/pubnubchat/fragment/ChatFragment.java
@@ -1,17 +1,32 @@
 package com.example.naresh.pubnubchat.fragment;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.databinding.DataBindingUtil;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.naresh.pubnubchat.R;
+import com.example.naresh.pubnubchat.callback.CustomCallback;
+import com.example.naresh.pubnubchat.databinding.ChatFragmentBinding;
 
 public class ChatFragment extends Fragment {
 
+    private ChatFragmentBinding mChatFragmentBinding;
+    private String TAG = ChatFragment.class.getSimpleName();
+    private SharedPreferences sharedPreferences;
+    private CustomCallback callback;
+    private Context context;
 
     public ChatFragment() {
         // Required empty public constructor
@@ -26,6 +41,40 @@ public class ChatFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat, container, false);
+        mChatFragmentBinding =
+                DataBindingUtil.inflate(inflater, R.layout.fragment_chat, container, false);
+        setHasOptionsMenu(true);
+
+        context = getActivity();
+        callback = (CustomCallback) context;
+        sharedPreferences = context.getSharedPreferences("details", Context.MODE_PRIVATE);
+        getActivity().setTitle("Chats");
+        return mChatFragmentBinding.getRoot();
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.logout, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.logout) {
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+            builder.setMessage("Do you really want to logout ??");
+            builder.setPositiveButton("Logout", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    callback.loginActivity(1);
+                    sharedPreferences.edit().remove("username").apply();
+                    Toast.makeText(getActivity(), "Logged out Successfully", Toast.LENGTH_SHORT).show();
+                }
+            });
+            builder.setNegativeButton("Cancel", null);
+            builder.show();
+            return true;
+        }
+        return false;
     }
 }
